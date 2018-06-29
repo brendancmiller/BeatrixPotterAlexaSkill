61617a6b2ca96830cc873ab5bc938578102b6301
Brendan Miller <brendancmiller@gmail.com>
2018-06-29 13:06:43 -0400

    initial code commit

---
+++ BeatrixPotterStoryAlexa/Function.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace BeatrixPotterStoryAlexa
+{
+    public class Function
+    {
+        //private ILambdaContext context = null;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
+        {
+            SkillResponse response = new SkillResponse();
+            response.Response = new ResponseBody();
+            response.Response.ShouldEndSession = false;
+            IOutputSpeech innerResponse = null;
+            var log = context.Logger;
+            log.LogLine($"Skill Request Object:");
+            log.LogLine(JsonConvert.SerializeObject(input));
+
+            if (input.GetRequestType() == typeof(LaunchRequest))
+            {
+                log.LogLine($"Default LaunchRequest made: 'Alexa, open Beatrix Potter");
+                innerResponse = new PlainTextOutputSpeech();
+                (innerResponse as PlainTextOutputSpeech).Text = StoryBook.GetStory().Body;
+
+            }
+            else if (input.GetRequestType() == typeof(IntentRequest))
+            {
+                var intentRequest = (IntentRequest)input.Request;
+
+                switch (intentRequest.Intent.Name)
+                {
+                    case "AMAZON.CancelIntent":
+                        log.LogLine($"AMAZON.CancelIntent: send StopMessage");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = "Goodbye!";
+                        response.Response.ShouldEndSession = true;
+                        break;
+                    case "AMAZON.StopIntent":
+                        log.LogLine($"AMAZON.StopIntent: send StopMessage");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = "Goodbye!";
+                        response.Response.ShouldEndSession = true;
+                        break;
+                    case "AMAZON.HelpIntent":
+                        log.LogLine($"AMAZON.HelpIntent: send HelpMessage");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = "You can say, tell me a story to start.";
+                        break;
+                    case "GetFactIntent":
+                        log.LogLine($"GetStory");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = StoryBook.GetStory().Body;
+                        break;
+                    default:
+                        log.LogLine($"Unknown intent: " + intentRequest.Intent.Name);
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = StoryBook.GetStory().Body;
+                        break;
+                }
+            }
+
+            response.Response.OutputSpeech = innerResponse;
+            response.Version = "1.0";
+            log.LogLine($"Skill Response Object...");
+            log.LogLine(JsonConvert.SerializeObject(response));
+            return response;
+
+        }
+    }
+}
---
+++ BeatrixPotterStoryAlexa/StoryBook.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace BeatrixPotterStoryAlexa
+{
+    public class StoryBook
+    {
+
+        public StoryBook(string locale)
+        {
+            this.Locale = locale;
+        }
+
+        public string Locale { get; set; }
+
+        public static List<Story> GetStories()
+        {
+            List<Story> stories = new List<Story>();
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load("TalesOfBeatrixPotter.xml");
+
+            foreach (XmlNode node in doc.DocumentElement.SelectSingleNode("/stories").ChildNodes)
+            {
+                string storyBody = node.InnerText;
+                string storyTitle = node.Attributes["title"]?.InnerText;
+
+                stories.Add(new Story(storyTitle, storyBody));
+            }
+
+
+
+
+            return stories;
+        }
+
+        public static Story GetStory()
+        {
+            List<Story> stories = GetStories();
+
+            Random rnd = new Random();
+            int storyNumber = rnd.Next(0, stories.Count -1);
+
+            //some of the stories are too long. this is a temporary hack until I can build in the functionality to allow for longer stories. Alexa only allows a maximum of 8000 character responses.
+            while (stories[storyNumber].Body.Length > 7000)
+            {
+                storyNumber = rnd.Next(0, stories.Count - 1);
+            }
+
+            return stories[storyNumber];
+        }
+    }
+
+    public class Story
+    {
+        public Story(string Title, string Body)
+        {
+            this.Title = Title;
+            this.Body = Body;
+
+        }
+
+        public string Body { get; set; }
+        public string Title { get; set; }
+
+    }
+
+}
